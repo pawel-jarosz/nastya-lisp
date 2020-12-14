b63e40c39541f3c1d341c30ef957c6bce0b9e445
Paweł Jarosz <jarosz.pawel@outlook.com>
2020-12-14 23:29:17 +0100

    Add ut for object storage

---
+++ src/LispExpression/ObjectStorageTest.cpp
@@ -0,0 +1,24 @@
+#include <memory>
+#include <string>
+
+#include <gtest/gtest.h>
+
+#include "LispExpression/ObjectStorage.hpp"
+#include "LispExpression/TypeSystem/StringObject.hpp"
+
+namespace nastya::lisp {
+
+TEST(ObjectStorageTest, testStorage)
+{
+    std::string test_case = "Dummy string";
+    std::unique_ptr<IObject> object(dynamic_cast<typesystem::StringObject*>(
+        new typesystem::StringObject(test_case)));
+    ObjectStorage storage(std::move(object));
+    EXPECT_EQ(storage.getType(), ObjectType::String);
+    EXPECT_EQ(storage.getRawObject().getType(), ObjectType::String);
+    EXPECT_EQ(dynamic_cast<typesystem::StringObject&>(storage.getRawObject())
+                  .getValue(),
+              test_case);
+}
+
+}  // namespace nastya::lisp

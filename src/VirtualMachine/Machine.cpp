a3ec2a6a6de78e960a5263bbf3f86c67a9cc4287
Paweł Jarosz <jarosz.pawel@outlook.com>
2020-12-27 15:35:35 +0100

    Fix execution procedure

--- src/VirtualMachine/Machine.cpp
+++ src/VirtualMachine/Machine.cpp
@@ -15,18 +15,21 @@ Machine::Machine(const modules::ModuleRegistry& registry) : m_modules{registry} 
 }
 
 lisp::ObjectStorage Machine::run(const lisp::ObjectStorage& list) {
-    std::cout << "Evaluate: " << list.toString() << std::endl;
     if (list.getType() != lisp::ObjectType::List) {
         return list;
     }
     auto raw_object = dynamic_cast<lisp::typesystem::ListObject&>(list.getRawObject());
     auto content = raw_object.getContent();
-    std::cout << "Label type: " << content[0].getType() << std::endl;
     auto label = dynamic_cast<lisp::typesystem::LabelObject&>(content[0].getRawObject());
     std::vector<lisp::ObjectStorage> arguments;
-    std::for_each(++content.begin(), content.end(), [&](const auto& obj){
-        arguments.emplace_back(run(obj));
-    });
+    if (label.getValue() == "Quote") {
+        arguments = std::vector(++content.begin(), content.end());
+    }
+    else {
+        std::for_each(++content.begin(), content.end(), [&](const auto& obj){
+          arguments.emplace_back(run(obj));
+        });
+    }
     std::unique_ptr<lisp::typesystem::ListObject> obj(new lisp::typesystem::ListObject(arguments));
     lisp::ObjectStorage argument(std::move(obj));
     return m_modules.getFunction(label.getValue()).evaluate(*this, argument);

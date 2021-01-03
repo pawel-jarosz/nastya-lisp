180cca6062e3ea5f6a5aec01197b5e10f27d0358
Paweł Jarosz <jarosz.pawel@outlook.com>
2021-01-03 18:25:03 +0100

    Add memory abstraction mock

---
+++ src/Runtime/Testing/MemoryMock.hpp
@@ -0,0 +1,13 @@
+//
+// Created by caedus on 03.01.2021.
+//
+
+#pragma once
+
+#include "Runtime/Interface/IMemory.hpp"
+
+namespace nastya::runtime {
+class MemoryMock : public IMemory {
+public:
+};
+}

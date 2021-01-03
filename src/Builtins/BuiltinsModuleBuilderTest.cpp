a484f1e146a48b83f17bef7b073fe22f43ed09c1
Paweł Jarosz <jarosz.pawel@outlook.com>
2021-01-03 18:25:30 +0100

    Add skeleton for builtins module builder test

---
+++ src/Builtins/BuiltinsModuleBuilderTest.cpp
@@ -0,0 +1,14 @@
+//
+// Created by caedus on 03.01.2021.
+//
+
+#include <gtest/gtest.h>
+
+namespace nastya::builtins {
+using namespace testing;
+
+TEST(BuiltinsModuleBuilderTest, simpleTest) {
+
+}
+
+}

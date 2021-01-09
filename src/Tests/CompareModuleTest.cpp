a649d581cb9a1a2d87c13c9267436bcfd5f710f3
Paweł Jarosz <jarosz.pawel@outlook.com>
2021-01-09 21:43:07 +0100

    add test for additional command

--- src/Tests/CompareModuleTest.cpp
+++ src/Tests/CompareModuleTest.cpp
@@ -86,5 +86,200 @@ TEST(CompareEvaluatorTest, testEqualFails) {
     }
 }
 
+TEST(CompareEvaluatorTest, testLowerSuccessStory)
+{
+    {
+        LowerEvaluator evaluator;
+        runtime::MemoryMock memory_mock;
+        lisp::testing::ListBuilder builder;
+        auto argument_list = builder.addNumber(2).addNumber(3).build();
+        auto result = evaluator.evaluate(memory_mock, argument_list);
+        EXPECT_EQ(result.toString(), "#true");
+    }
+    {
+        LowerEvaluator evaluator;
+        runtime::MemoryMock memory_mock;
+        lisp::testing::ListBuilder builder;
+        auto argument_list = builder.addNumber(2).addNumber(2).build();
+        auto result = evaluator.evaluate(memory_mock, argument_list);
+        EXPECT_EQ(result.toString(), "#false");
+    }
+    {
+        LowerEvaluator evaluator;
+        runtime::MemoryMock memory_mock;
+        lisp::testing::ListBuilder builder;
+        auto argument_list = builder.addNumber(4).addNumber(3).build();
+        auto result = evaluator.evaluate(memory_mock, argument_list);
+        EXPECT_EQ(result.toString(), "#false");
+    }
+}
+
+TEST(CompareEvaluatorTest, testLowerFails) {
+    LowerEvaluator evaluator;
+    runtime::MemoryMock memory_mock;
+    {
+        // Evaluator receives as an argument not a list
+        lisp::ObjectStorage argument_list(std::unique_ptr<lisp::IObject>(
+            new lisp::typesystem::NumberObject(2)));
+        EXPECT_THROW(evaluator.evaluate(memory_mock, argument_list), BuiltinsException);
+    }
+    {
+        lisp::testing::ListBuilder builder;
+        auto not_enough_arguments = builder.addNumber(2).build();
+        EXPECT_THROW(evaluator.evaluate(memory_mock, not_enough_arguments), BuiltinsException);
+    }
+    {
+        lisp::testing::ListBuilder builder;
+        auto too_many_arguments = builder.addNumber(2).addNumber(3).addNumber(4).build();
+        EXPECT_THROW(evaluator.evaluate(memory_mock, too_many_arguments), BuiltinsException);
+    }
+}
+
+TEST(CompareEvaluatorTest, testLowerOrEqualSuccessStory)
+{
+    {
+        LowerOrEqualEvaluator evaluator;
+        runtime::MemoryMock memory_mock;
+        lisp::testing::ListBuilder builder;
+        auto argument_list = builder.addNumber(2).addNumber(3).build();
+        auto result = evaluator.evaluate(memory_mock, argument_list);
+        EXPECT_EQ(result.toString(), "#true");
+    }
+    {
+        LowerOrEqualEvaluator evaluator;
+        runtime::MemoryMock memory_mock;
+        lisp::testing::ListBuilder builder;
+        auto argument_list = builder.addNumber(2).addNumber(2).build();
+        auto result = evaluator.evaluate(memory_mock, argument_list);
+        EXPECT_EQ(result.toString(), "#true");
+    }
+    {
+        LowerOrEqualEvaluator evaluator;
+        runtime::MemoryMock memory_mock;
+        lisp::testing::ListBuilder builder;
+        auto argument_list = builder.addNumber(4).addNumber(3).build();
+        auto result = evaluator.evaluate(memory_mock, argument_list);
+        EXPECT_EQ(result.toString(), "#false");
+    }
+}
+
+TEST(CompareEvaluatorTest, testLowerOrEqualFails) {
+    LowerOrEqualEvaluator evaluator;
+    runtime::MemoryMock memory_mock;
+    {
+        // Evaluator receives as an argument not a list
+        lisp::ObjectStorage argument_list(std::unique_ptr<lisp::IObject>(
+            new lisp::typesystem::NumberObject(2)));
+        EXPECT_THROW(evaluator.evaluate(memory_mock, argument_list), BuiltinsException);
+    }
+    {
+        lisp::testing::ListBuilder builder;
+        auto not_enough_arguments = builder.addNumber(2).build();
+        EXPECT_THROW(evaluator.evaluate(memory_mock, not_enough_arguments), BuiltinsException);
+    }
+    {
+        lisp::testing::ListBuilder builder;
+        auto too_many_arguments = builder.addNumber(2).addNumber(3).addNumber(4).build();
+        EXPECT_THROW(evaluator.evaluate(memory_mock, too_many_arguments), BuiltinsException);
+    }
+}
+
+TEST(CompareEvaluatorTest, testGreaterOrEqualSuccessStory)
+{
+    {
+        GreaterOrEqualEvaluator evaluator;
+        runtime::MemoryMock memory_mock;
+        lisp::testing::ListBuilder builder;
+        auto argument_list = builder.addNumber(2).addNumber(3).build();
+        auto result = evaluator.evaluate(memory_mock, argument_list);
+        EXPECT_EQ(result.toString(), "#false");
+    }
+    {
+        GreaterOrEqualEvaluator evaluator;
+        runtime::MemoryMock memory_mock;
+        lisp::testing::ListBuilder builder;
+        auto argument_list = builder.addNumber(2).addNumber(2).build();
+        auto result = evaluator.evaluate(memory_mock, argument_list);
+        EXPECT_EQ(result.toString(), "#true");
+    }
+    {
+        GreaterOrEqualEvaluator evaluator;
+        runtime::MemoryMock memory_mock;
+        lisp::testing::ListBuilder builder;
+        auto argument_list = builder.addNumber(4).addNumber(3).build();
+        auto result = evaluator.evaluate(memory_mock, argument_list);
+        EXPECT_EQ(result.toString(), "#true");
+    }
+}
+
+TEST(CompareEvaluatorTest, testGreaterOrEqualFails) {
+    GreaterOrEqualEvaluator evaluator;
+    runtime::MemoryMock memory_mock;
+    {
+        // Evaluator receives as an argument not a list
+        lisp::ObjectStorage argument_list(std::unique_ptr<lisp::IObject>(
+            new lisp::typesystem::NumberObject(2)));
+        EXPECT_THROW(evaluator.evaluate(memory_mock, argument_list), BuiltinsException);
+    }
+    {
+        lisp::testing::ListBuilder builder;
+        auto not_enough_arguments = builder.addNumber(2).build();
+        EXPECT_THROW(evaluator.evaluate(memory_mock, not_enough_arguments), BuiltinsException);
+    }
+    {
+        lisp::testing::ListBuilder builder;
+        auto too_many_arguments = builder.addNumber(2).addNumber(3).addNumber(4).build();
+        EXPECT_THROW(evaluator.evaluate(memory_mock, too_many_arguments), BuiltinsException);
+    }
+}
+
+TEST(CompareEvaluatorTest, testGreaterSuccessStory)
+{
+    {
+        GreaterOrEqualEvaluator evaluator;
+        runtime::MemoryMock memory_mock;
+        lisp::testing::ListBuilder builder;
+        auto argument_list = builder.addNumber(2).addNumber(3).build();
+        auto result = evaluator.evaluate(memory_mock, argument_list);
+        EXPECT_EQ(result.toString(), "#false");
+    }
+    {
+        GreaterOrEqualEvaluator evaluator;
+        runtime::MemoryMock memory_mock;
+        lisp::testing::ListBuilder builder;
+        auto argument_list = builder.addNumber(2).addNumber(2).build();
+        auto result = evaluator.evaluate(memory_mock, argument_list);
+        EXPECT_EQ(result.toString(), "#true");
+    }
+    {
+        GreaterOrEqualEvaluator evaluator;
+        runtime::MemoryMock memory_mock;
+        lisp::testing::ListBuilder builder;
+        auto argument_list = builder.addNumber(4).addNumber(3).build();
+        auto result = evaluator.evaluate(memory_mock, argument_list);
+        EXPECT_EQ(result.toString(), "#true");
+    }
+}
+
+TEST(CompareEvaluatorTest, testGreaterFails) {
+    GreaterEvaluator evaluator;
+    runtime::MemoryMock memory_mock;
+    {
+        // Evaluator receives as an argument not a list
+        lisp::ObjectStorage argument_list(std::unique_ptr<lisp::IObject>(
+            new lisp::typesystem::NumberObject(2)));
+        EXPECT_THROW(evaluator.evaluate(memory_mock, argument_list), BuiltinsException);
+    }
+    {
+        lisp::testing::ListBuilder builder;
+        auto not_enough_arguments = builder.addNumber(2).build();
+        EXPECT_THROW(evaluator.evaluate(memory_mock, not_enough_arguments), BuiltinsException);
+    }
+    {
+        lisp::testing::ListBuilder builder;
+        auto too_many_arguments = builder.addNumber(2).addNumber(3).addNumber(4).build();
+        EXPECT_THROW(evaluator.evaluate(memory_mock, too_many_arguments), BuiltinsException);
+    }
+}
 
 }

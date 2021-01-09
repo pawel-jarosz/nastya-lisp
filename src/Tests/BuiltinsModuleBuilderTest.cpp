bd83714fad6394265ede20d901f486b8d4251092
Paweł Jarosz <jarosz.pawel@outlook.com>
2021-01-09 20:41:29 +0100

    Improve test to verify compare module

--- src/Tests/BuiltinsModuleBuilderTest.cpp
+++ src/Tests/BuiltinsModuleBuilderTest.cpp
@@ -8,15 +8,25 @@
 #include "Modules/ModuleRegistry.hpp"
 
 namespace nastya::builtins {
-using namespace testing;
+using namespace ::testing;
 
-TEST(BuiltinsModuleBuilderTest, verifyBuilder) {
+struct BuiltinsModuleBuilderTest : public Test {
+    BuiltinsModuleBuilderTest() : registry(), builder(registry) {
+        builder.build();
+    }
+
     modules::ModuleRegistry registry;
-    BuiltinsModuleBuilder builder(registry);
-    builder.build();
+    BuiltinsModuleBuilder builder;
+};
+
+TEST_F(BuiltinsModuleBuilderTest, basicExpectation) {
     auto registered_modules = registry.getAvailableModules();
-    EXPECT_EQ(registered_modules.size(), 1);
-    EXPECT_EQ(registered_modules[0], "Lang.Lists");
+    EXPECT_EQ(registered_modules.size(), 2);
+    EXPECT_EQ(registered_modules[0], "Lang.Compare");
+    EXPECT_EQ(registered_modules[1], "Lang.Lists");
+}
+
+TEST_F(BuiltinsModuleBuilderTest, verifyListsModule) {
     EXPECT_TRUE(registry.isAvailableFunction("Head"));
     EXPECT_TRUE(registry.isAvailableFunction("Tail"));
     EXPECT_TRUE(registry.isAvailableFunction("Quote"));
@@ -24,6 +34,13 @@ TEST(BuiltinsModuleBuilderTest, verifyBuilder) {
     EXPECT_EQ(registry.getFunction("Head").getName(), "Head");
     EXPECT_EQ(registry.getFunction("Tail").getName(), "Tail");
     EXPECT_EQ(registry.getFunction("Quote").getName(), "Quote");
+}
+
+TEST_F(BuiltinsModuleBuilderTest, verifyCompareModule) {
+    EXPECT_TRUE(registry.isAvailableFunction("Equal"));
+    EXPECT_TRUE(registry.isAvailableFunction("Compare"));
+    EXPECT_EQ(registry.getFunction("Equal").getName(), "Equal");
+    EXPECT_EQ(registry.getFunction("Compare").getName(), "Compare");
 }
 
 }

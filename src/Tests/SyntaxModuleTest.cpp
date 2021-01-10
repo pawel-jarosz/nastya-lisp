ead8b8e6a786d4be42d4e68c6a6c570bbc58d140
Paweł Jarosz <jarosz.pawel@outlook.com>
2021-01-10 16:04:23 +0100

    Add tests for cond method

--- src/Tests/SyntaxModuleTest.cpp
+++ src/Tests/SyntaxModuleTest.cpp
@@ -67,4 +67,62 @@ TEST(SyntaxEvaluatorTest, testIfFails) {
     }
 }
 
+TEST(SyntaxEvaluatorTest, testCondName) {
+    CondEvaluator evaluator;
+    EXPECT_EQ(evaluator.getName(), "Cond");
+}
+
+TEST(SyntaxEvaluatorTest, testCondSuccess) {
+    CondEvaluator evaluator;
+    runtime::MemoryMock memory_mock;
+    {
+        // With else on begin
+        lisp::testing::ListBuilder builder;
+        auto argument_list = builder
+                             .openList()
+                             .addLabel("Else")
+                             .addNumber(2)
+                             .closeList()
+                             .build();
+        auto result = evaluator.evaluate(memory_mock, argument_list);
+        EXPECT_EQ(result.toString(), "2");
+    }
+    {
+        // Omit false condition and return true
+        lisp::testing::ListBuilder builder;
+        auto argument_list = builder
+            .openList()
+            .addBoolean(false)
+            .addNumber(2)
+            .closeList()
+            .openList()
+            .addBoolean(true)
+            .addNumber(3)
+            .closeList()
+            .build();
+        auto result = evaluator.evaluate(memory_mock, argument_list);
+        EXPECT_EQ(result.toString(), "3");
+    }
+    {
+        // Omit false conditions and use else
+        lisp::testing::ListBuilder builder;
+        auto argument_list = builder
+            .openList()
+            .addBoolean(false)
+            .addNumber(2)
+            .closeList()
+            .openList()
+            .addBoolean(false)
+            .addNumber(3)
+            .closeList()
+            .openList()
+            .addLabel("Else")
+            .addNumber(4)
+            .closeList()
+            .build();
+        auto result = evaluator.evaluate(memory_mock, argument_list);
+        EXPECT_EQ(result.toString(), "4");
+    }
+}
+
 }

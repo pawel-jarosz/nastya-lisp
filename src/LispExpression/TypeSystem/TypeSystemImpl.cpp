7ef1fe05a67efe67bf66396981d07f7a5748aafb
Paweł Jarosz <jarosz.pawel@outlook.com>
2021-01-19 19:51:54 +0100

    add lambda object

--- src/LispExpression/TypeSystem/TypeSystemImpl.cpp
+++ src/LispExpression/TypeSystem/TypeSystemImpl.cpp
@@ -1,14 +1,14 @@
 #include <sstream>
 #include <utility>
 
+#include "LispExpression/TypeSystem/GenericObject.hpp"
 #include "LispExpression/TypeSystem/BooleanObject.hpp"
 #include "LispExpression/TypeSystem/LabelObject.hpp"
 #include "LispExpression/TypeSystem/ListObject.hpp"
 #include "LispExpression/TypeSystem/NumberObject.hpp"
 #include "LispExpression/TypeSystem/StringObject.hpp"
 #include "LispExpression/TypeSystem/TypeSystemError.hpp"
-#include "GenericObject.hpp"
-
+#include "LispExpression/TypeSystem/LambdaObject.hpp"
 
 namespace nastya::lisp::typesystem {
 
@@ -344,6 +344,68 @@ std::string ListObject::info() const
     std::stringstream ss;
     ss << "List => " << toString();
     return ss.str();
+}
+
+LambdaObject::LambdaObject(const ListObject& argumentsList, const ListObject& command)
+: m_arguments(argumentsList.getContent())
+, m_command(command.getContent())
+{
+}
+
+LambdaObject::LambdaObject(const LambdaObject& rhs)
+: m_arguments{rhs.getArgumentsList().getContent()}
+, m_command{rhs.getCommand().getContent()}
+{
+}
+
+LambdaObject::LambdaObject(LambdaObject&& rhs)
+{
+    m_arguments = std::move(rhs.m_arguments);
+    m_command = std::move(rhs.m_command);
+}
+
+LambdaObject& LambdaObject::operator=(const LambdaObject& rhs)
+{
+    m_arguments = rhs.m_arguments;
+    m_command = rhs.m_command;
+    return *this;
+}
+LambdaObject& LambdaObject::operator=(LambdaObject&& rhs)
+{
+    m_arguments = std::move(rhs.m_arguments);
+    m_command = std::move(rhs.m_command);
+    return *this;
+}
+
+ObjectType LambdaObject::getType() const
+{
+    return ObjectType::Lambda;
+}
+
+IObject* LambdaObject::clone() const
+{
+    return new LambdaObject(m_arguments, m_command);
+}
+
+std::string LambdaObject::toString() const
+{
+    std::stringstream ss;
+    ss << "Lambda => args:" << m_arguments.toString();
+    return std::string();
+}
+
+std::string LambdaObject::info() const
+{
+    return toString();
+}
+const ListObject& LambdaObject::getArgumentsList() const
+{
+    return m_arguments;
+}
+
+const ListObject& LambdaObject::getCommand() const
+{
+    return m_command;
 }
 
 }  // namespace nastya::lisp::typesystem

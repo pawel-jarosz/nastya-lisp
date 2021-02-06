32034892a52dabce5507e0c90ae7e16dde252cf8
Paweł Jarosz <jarosz.pawel@outlook.com>
2021-02-06 12:32:23 +0100

    Add builtins builder

--- src/Builtins/BuiltinsBuilder.hpp
+++ src/Builtins/BuiltinsBuilder.hpp
@@ -5,17 +5,19 @@
 #pragma once
 
 #include "Modules/ModuleRegistry.hpp"
+#include "Builtins/Interface/IBuiltinsModuleFactory.hpp"
 
 namespace nastya::builtins {
 
-class BuiltinsModuleBuilder
+class BuiltinsBuilder
 {
 public:
-    BuiltinsModuleBuilder(modules::ModuleRegistry& registry);
+    BuiltinsBuilder(modules::ModuleRegistry& registry, IBuiltinsModuleFactory& factory);
     void build() const;
 
 private:
     modules::ModuleRegistry& m_registry;
+    IBuiltinsModuleFactory& m_factory;
 };
 
 }  // namespace nastya::builtins

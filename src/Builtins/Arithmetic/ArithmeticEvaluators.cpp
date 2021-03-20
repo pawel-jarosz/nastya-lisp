d697f622d1542f7db033c4d1257cdbd963410d51
Paweł Jarosz <jarosz.pawel@outlook.com>
2021-03-20 17:09:36 +0100

    Improve arithmetic implementation

--- src/Builtins/Arithmetic/ArithmeticEvaluators.cpp
+++ src/Builtins/Arithmetic/ArithmeticEvaluators.cpp
@@ -18,70 +18,155 @@ namespace nastya::builtins::arithmetic {
 
 using namespace utils;
 
+enum class ResultType {
+    Floating,
+    Integer,
+    Unknown
+};
+
+class Sumator {
+public:
+    Sumator() = default;
+    Sumator& operator+(int integer);
+    Sumator& operator+(float floating);
+    typesystem::NumberObject getResult() const;
+private:
+    union {
+        int integer_sum = 0;
+        float float_sum;
+    };
+    ResultType sum_result = ResultType::Integer;
+};
+
+Sumator& Sumator::operator+(int integer)
+{
+    if (sum_result == ResultType::Integer) {
+        integer_sum += integer;
+    }
+    else {
+        float_sum += integer;
+    }
+    return *this;
+}
+
+Sumator& Sumator::operator+(float floating)
+{
+    if (sum_result == ResultType::Integer) {
+        sum_result = ResultType::Floating;
+        float temp = integer_sum;
+        temp += floating;
+        float_sum = temp;
+    }
+    else {
+        float_sum += floating;
+    }
+    return *this;
+}
+
+typesystem::NumberObject Sumator::getResult() const
+{
+    if (sum_result == ResultType::Integer) {
+        return typesystem::NumberObject(integer_sum);
+    }
+    return typesystem::NumberObject(float_sum);
+}
+
 typesystem::ObjectStorage AddEvaluator::evaluate(runtime::IMemory&, const typesystem::ObjectStorage& object) const
 {
     const auto& arguments_list = Cast::as_list(object, "Lang.Arithmetic.Add expects list of arguments");
-    bool is_integer_sum = true;
-    union
-    {
-        int integer_sum = 0;
-        float float_sum;
-    } sum;
+    Sumator sum;
 
     for (const auto number_object : arguments_list.getContent())
     {
         const auto& number = utils::Cast::as_number(number_object, "Lang.Arithmetic.Add expects number");
-        if (is_integer_sum and number.getNumberType() == typesystem::NumberType::Floating)
+        if (number.getNumberType() == typesystem::NumberType::Floating)
         {
-            sum.float_sum = sum.integer_sum;
-            is_integer_sum = false;
+            sum = sum + number.getFloating();
         }
-        if (is_integer_sum)
-        {
-            sum.integer_sum += number.getInteger();
-        }
-        else
-        {
-            sum.float_sum += number.getFloating();
+        else {
+            sum = sum + number.getInteger();
         }
     }
-    auto result_pointer = (is_integer_sum ? (new typesystem::NumberObject(sum.integer_sum))
-                                          : (new typesystem::NumberObject(sum.float_sum)));
-    typesystem::ObjectStorage result{std::unique_ptr<typesystem::IObject>(result_pointer)};
+
+    typesystem::ObjectStorage result{std::unique_ptr<typesystem::IObject>(sum.getResult().clone())};
     return result;
+}
+
+class Subtractor {
+public:
+    Subtractor() = default;
+    Subtractor& operator-(int integer);
+    Subtractor& operator-(float floating);
+    typesystem::NumberObject getResult() const;
+private:
+    union {
+        int integer_subtraction = 0;
+        float float_subtraction;
+    };
+    ResultType subtraction_result = ResultType::Unknown;
+};
+
+Subtractor& Subtractor::operator-(int integer)
+{
+    if (subtraction_result == ResultType::Unknown) {
+        integer_subtraction = integer;
+        subtraction_result = ResultType::Integer;
+        return *this;
+    }
+    if (subtraction_result == ResultType::Integer) {
+        integer_subtraction -= integer;
+    }
+    else {
+        float_subtraction -= integer;
+    }
+    return *this;
+}
+
+Subtractor& Subtractor::operator-(float floating)
+{
+    if (subtraction_result == ResultType::Unknown) {
+        float_subtraction = floating;
+        subtraction_result = ResultType::Floating;
+        return *this;
+    }
+    if (subtraction_result == ResultType::Integer) {
+        subtraction_result = ResultType::Floating;
+        float temp = integer_subtraction;
+        temp -= floating;
+        float_subtraction = temp;
+    }
+    else {
+        float_subtraction -= floating;
+    }
+    return *this;
+}
+
+typesystem::NumberObject Subtractor::getResult() const
+{
+    if (subtraction_result == ResultType::Integer) {
+        return typesystem::NumberObject(integer_subtraction);
+    }
+    return typesystem::NumberObject(float_subtraction);
 }
 
 typesystem::ObjectStorage
 SubtractionEvaluator::evaluate(runtime::IMemory&, const typesystem::ObjectStorage& object) const
 {
     const auto& arguments_list = Cast::as_list(object, "Lang.Arithmetic.Add expects list of arguments");
-    bool is_integer_sum = true;
-    union
-    {
-        int integer_sum = 0;
-        float float_sum;
-    } sum;
+    Subtractor subtractor;
 
     for (const auto number_object : arguments_list.getContent())
     {
         const auto& number = utils::Cast::as_number(number_object, "Lang.Arithmetic.Add expects number");
-        if (is_integer_sum and number.getNumberType() == typesystem::NumberType::Floating)
+        if (number.getNumberType() == typesystem::NumberType::Floating)
         {
-            sum.float_sum = sum.integer_sum;
-            is_integer_sum = false;
+            subtractor = subtractor - number.getFloating();
         }
-        if (is_integer_sum)
-        {
-            sum.integer_sum -= number.getInteger();
-        }
-        else
-        {
-            sum.float_sum -= number.getFloating();
+        else {
+            subtractor = subtractor - number.getInteger();
         }
     }
-    auto result_pointer = (is_integer_sum ? (new typesystem::NumberObject(sum.integer_sum))
-                                          : (new typesystem::NumberObject(sum.float_sum)));
-    typesystem::ObjectStorage result{std::unique_ptr<typesystem::IObject>(result_pointer)};
+    typesystem::ObjectStorage result{std::unique_ptr<typesystem::IObject>(subtractor.getResult().clone())};
     return result;
 }
 

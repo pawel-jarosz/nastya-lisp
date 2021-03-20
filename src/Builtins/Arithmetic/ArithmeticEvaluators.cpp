10bf19607052bb0eec7e5759f427538e7600b1df
Paweł Jarosz <jarosz.pawel@outlook.com>
2021-03-20 17:43:12 +0100

    add multiply and division

--- src/Builtins/Arithmetic/ArithmeticEvaluators.cpp
+++ src/Builtins/Arithmetic/ArithmeticEvaluators.cpp
@@ -170,4 +170,150 @@ SubtractionEvaluator::evaluate(runtime::IMemory&, const typesystem::ObjectStorag
     return result;
 }
 
+class Multiplier {
+public:
+    Multiplier() = default;
+    Multiplier& operator*(int integer);
+    Multiplier& operator*(float floating);
+    typesystem::NumberObject getResult() const;
+private:
+    union {
+        int integer_product = 1;
+        float floating_product;
+    };
+    ResultType product_result = ResultType::Integer;
+};
+
+Multiplier& Multiplier::operator*(int integer)
+{
+    if (product_result == ResultType::Integer) {
+        integer_product *= integer;
+    }
+    else {
+        floating_product *= integer;
+    }
+    return *this;
+}
+
+Multiplier& Multiplier::operator*(float floating)
+{
+    if (product_result == ResultType::Integer) {
+        product_result = ResultType::Floating;
+        float temp = integer_product;
+        temp *= floating;
+        floating_product = temp;
+    }
+    else {
+        floating_product *= floating;
+    }
+    return *this;
+}
+
+typesystem::NumberObject Multiplier::getResult() const
+{
+    if (product_result == ResultType::Integer) {
+        return typesystem::NumberObject(integer_product);
+    }
+    return typesystem::NumberObject(floating_product);
+}
+
+typesystem::ObjectStorage MultiplyEvaluator::evaluate(runtime::IMemory&, const typesystem::ObjectStorage& object) const
+{
+    const auto& arguments_list = Cast::as_list(object, "Lang.Arithmetic.Multiply expects list of arguments");
+    Multiplier multiplier;
+
+    for (const auto number_object : arguments_list.getContent())
+    {
+        const auto& number = utils::Cast::as_number(number_object, "Lang.Arithmetic.Multiply expects number");
+        if (number.getNumberType() == typesystem::NumberType::Floating)
+        {
+            multiplier = multiplier * number.getFloating();
+        }
+        else {
+            multiplier = multiplier * number.getInteger();
+        }
+    }
+
+    typesystem::ObjectStorage result{std::unique_ptr<typesystem::IObject>(multiplier.getResult().clone())};
+    return result;
+}
+
+class Divisoner {
+public:
+    Divisoner() = default;
+    Divisoner& operator/(int integer);
+    Divisoner& operator/(float floating);
+    typesystem::NumberObject getResult() const;
+private:
+    union {
+        int integer_product = 0;
+        float float_product;
+    };
+    ResultType division_result = ResultType::Unknown;
+};
+
+Divisoner& Divisoner::operator/(int integer)
+{
+    if (division_result == ResultType::Unknown) {
+        integer_product = integer;
+        division_result = ResultType::Integer;
+        return *this;
+    }
+    if (division_result == ResultType::Integer) {
+        integer_product /= integer;
+    }
+    else {
+        float_product /= integer;
+    }
+    return *this;
+}
+
+Divisoner& Divisoner::operator/(float floating)
+{
+    if (division_result == ResultType::Unknown) {
+        float_product = floating;
+        division_result = ResultType::Floating;
+        return *this;
+    }
+    if (division_result == ResultType::Integer) {
+        division_result = ResultType::Floating;
+        float temp = integer_product;
+        temp /= floating;
+        float_product = temp;
+    }
+    else {
+        float_product /= floating;
+    }
+    return *this;
+}
+
+typesystem::NumberObject Divisoner::getResult() const
+{
+    if (division_result == ResultType::Integer) {
+        return typesystem::NumberObject(integer_product);
+    }
+    return typesystem::NumberObject(float_product);
+}
+
+typesystem::ObjectStorage
+DivisionEvaluator::evaluate(runtime::IMemory&, const typesystem::ObjectStorage& object) const
+{
+    const auto& arguments_list = Cast::as_list(object, "Lang.Arithmetic.Add expects list of arguments");
+    Divisoner divisioner;
+
+    for (const auto number_object : arguments_list.getContent())
+    {
+        const auto& number = utils::Cast::as_number(number_object, "Lang.Arithmetic.Add expects number");
+        if (number.getNumberType() == typesystem::NumberType::Floating)
+        {
+            divisioner = divisioner / number.getFloating();
+        }
+        else {
+            divisioner = divisioner / number.getInteger();
+        }
+    }
+    typesystem::ObjectStorage result{std::unique_ptr<typesystem::IObject>(divisioner.getResult().clone())};
+    return result;
+}
+
 }  // namespace nastya::builtins::arithmetic
--- src/Tests/Builtins/ArithmeticModuleTest.cpp
+++ src/Tests/Builtins/ArithmeticModuleTest.cpp
@@ -116,4 +116,96 @@ TEST(ArithmeticEvaluatorTest, subtractionOfMixedList2)
     EXPECT_EQ(result.toString(), "-4.3");
 }
 
+TEST(ArithmeticEvaluatorTest, testMultiplyName)
+{
+    MultiplyEvaluator evaluator;
+    EXPECT_EQ(evaluator.getName(), "*");
+}
+
+TEST(ArithmeticEvaluatorTest, productOfIntegerList)
+{
+    MultiplyEvaluator evaluator;
+    runtime::MemoryMock memory_mock;
+    ListBuilder builder;
+    auto argument_list = builder.addNumber(2).addNumber(3).addNumber(4).build();
+    auto result = evaluator.evaluate(memory_mock, argument_list);
+    EXPECT_EQ(result.toString(), "24");
+}
+
+TEST(ArithmeticEvaluatorTest, productOfFloatingList)
+{
+    MultiplyEvaluator evaluator;
+    runtime::MemoryMock memory_mock;
+    ListBuilder builder;
+    auto argument_list = builder.addNumber(2.4f).addNumber(0.1f).addNumber(2.5f).build();
+    auto result = evaluator.evaluate(memory_mock, argument_list);
+    EXPECT_EQ(result.toString(), "0.6");
+}
+
+TEST(ArithmeticEvaluatorTest, productOfMixedList1)
+{
+    MultiplyEvaluator evaluator;
+    runtime::MemoryMock memory_mock;
+    ListBuilder builder;
+    auto argument_list = builder.addNumber(1.4f).addNumber(2).addNumber(0.3f).build();
+    auto result = evaluator.evaluate(memory_mock, argument_list);
+    EXPECT_EQ(result.toString(), "0.84");
+}
+
+TEST(ArithmeticEvaluatorTest, productOfMixedList2)
+{
+    MultiplyEvaluator evaluator;
+    runtime::MemoryMock memory_mock;
+    ListBuilder builder;
+    auto argument_list = builder.addNumber(1).addNumber(2.3f).addNumber(3).build();
+    auto result = evaluator.evaluate(memory_mock, argument_list);
+    EXPECT_EQ(result.toString(), "6.9");
+}
+
+TEST(ArithmeticEvaluatorTest, testDivisionName)
+{
+    DivisionEvaluator evaluator;
+    EXPECT_EQ(evaluator.getName(), "/");
+}
+
+TEST(ArithmeticEvaluatorTest, divisionProductOfIntegerList)
+{
+    DivisionEvaluator evaluator;
+    runtime::MemoryMock memory_mock;
+    ListBuilder builder;
+    auto argument_list = builder.addNumber(24).addNumber(6).addNumber(2).build();
+    auto result = evaluator.evaluate(memory_mock, argument_list);
+    EXPECT_EQ(result.toString(), "2");
+}
+
+TEST(ArithmeticEvaluatorTest, divisionProductOfFloatingList)
+{
+    DivisionEvaluator evaluator;
+    runtime::MemoryMock memory_mock;
+    ListBuilder builder;
+    auto argument_list = builder.addNumber(2.4f).addNumber(0.1f).addNumber(2.5f).build();
+    auto result = evaluator.evaluate(memory_mock, argument_list);
+    EXPECT_EQ(result.toString(), "9.6");
+}
+
+TEST(ArithmeticEvaluatorTest, divisionProductOfMixedList1)
+{
+    DivisionEvaluator evaluator;
+    runtime::MemoryMock memory_mock;
+    ListBuilder builder;
+    auto argument_list = builder.addNumber(1.4f).addNumber(2).addNumber(0.1f).build();
+    auto result = evaluator.evaluate(memory_mock, argument_list);
+    EXPECT_EQ(result.toString(), "7");
+}
+
+TEST(ArithmeticEvaluatorTest, divisionProductOfMixedList2)
+{
+    DivisionEvaluator evaluator;
+    runtime::MemoryMock memory_mock;
+    ListBuilder builder;
+    auto argument_list = builder.addNumber(1).addNumber(0.1f).addNumber(2).build();
+    auto result = evaluator.evaluate(memory_mock, argument_list);
+    EXPECT_EQ(result.toString(), "5");
+}
+
 }
